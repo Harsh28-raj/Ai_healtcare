9fb6917ecc30faac0c9e2f979c8401dc7b9a1b08
Harsh Raj <harshraj4308g@gmail.com>
2026-04-12 18:24:57 +0530

    fix text splitter import

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ from typing import Optional
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from groq import Groq
 from dotenv import load_dotenv
