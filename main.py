8a0f737c4902b7115c4b2dcbb24a6d703fcb1c6a
Harsh Raj <harshraj4308g@gmail.com>
2026-04-18 02:08:16 +0530

    Revert "feat: food barcode + MongoDB"
    This reverts commit bb04f8bfe6b600abdebafa607de36f289b2d766d.

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-from fastapi import FastAPI, Query
-from database import food_log_collection, check_and_create_alerts
-from datetime import datetime
+from fastapi import FastAPI
 from pydantic import BaseModel
 from rapidfuzz import fuzz, process
 import json
@@ -21,7 +19,7 @@ class LogRequest(BaseModel):
     text: str
 
 @app.post("/analyze-log")
-async def analyze_log(request: LogRequest, user_id: str = Query(default="guest")):
+async def analyze_log(request: LogRequest):
     user_text = request.text.lower()
     user_words = user_text.split()
     
@@ -76,18 +74,6 @@ async def analyze_log(request: LogRequest, user_id: str = Query(default="guest")
         recommendation = "Watch out for salt and fried items in your next meal."
     elif activity_level != "Active":
         recommendation = "Try to add at least 15 mins of movement today."
-
-    food_log_collection.insert_one({
-        "user_id": user_id,
-        "items": list(detected_items.keys()),
-        "calories": total_calories,
-        "health_score": health_score,
-        "risk_flags": list(risk_flags),
-        "activity_level": activity_level,
-        "recommendation": recommendation,
-        "timestamp": datetime.utcnow()
-    })
-    check_and_create_alerts(user_id)
 
     return {
         "status": "success",
